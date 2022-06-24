aa15684c2ff1f8f2544722b114517543e1f9a60b
Debora Correa <deboragomescorr@gmail.com>
2022-06-24 16:03:48 -0300

    feat: register and login requests made to api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import App from "./App";
@@ -9,6 +12,19 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        transition={Slide}
+        theme="dark"
+      />
       <App />
     </BrowserRouter>
   </React.StrictMode>
